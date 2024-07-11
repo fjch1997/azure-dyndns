99c45aa152b5b9bd3a12df4b002abe2cfa4534e2
Jingchao Feng <fjch1997@outlook.com>
2024-07-10 23:39:19 -0700

    Obtain address from interface

--- net/Program.cs
+++ net/Program.cs
@@ -1,19 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Net;
 using System.Net.Http;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
-using Azure.Identity;
 using CommandLine;
 using Microsoft.Azure.Management.Dns;
 using Microsoft.Azure.Management.Dns.Models;
-using Microsoft.Rest;
 using Microsoft.Rest.Azure.Authentication;
 
 namespace AzureDynDns
 {
+    enum InterfaceAddressFamily
+    {
+        Any,
+        IPv4,
+        IPv6,
+    }
+
     class Program
     {
         public class Options
@@ -42,6 +51,15 @@ namespace AzureDynDns
             [JsonPropertyName("clientSecret")]
             [Option('x', "client-secret", HelpText = "Azure service principal client secret (or set AZURE_CLIENT_SECRET)")]
             public string ClientSecret { get; set; }
+            [JsonPropertyName("interfaceName")]
+            [Option('i', "interface-name", HelpText = "The network interface to obtain the IP address from. If emtpy, obtain the address from ipconfig.me.")]
+            public IList<string> InterfaceNames { get; set; }
+            [JsonPropertyName("iterfaceAddressFamilies")]
+            [Option('a', "interface-address-family", HelpText = "When the interface-name parameter is specified, use this parameter to limit the address family obtained from the interface. Possible values are IPv4, IPv6 and Any.", Required = false)]
+            public IList<InterfaceAddressFamily> InterfaceAddressFamilies { get; set; }
+            [JsonPropertyName("ttl")]
+            [Option('l', "TTL", HelpText = "A Time-to-live value for the DNS records.", Default = 3600)]
+            public long TTL { get; set; }
         }
         public static async Task Main(string[] args)
         {
@@ -56,28 +74,99 @@ namespace AzureDynDns
                 options = JsonSerializer.Deserialize<Options>(configJson);
             }
 
+            var aRecordSet = new RecordSet();
+            aRecordSet.ARecords = new List<ARecord>();
+            var aaaaRecordSet = new RecordSet();
+            aaaaRecordSet.AaaaRecords = new List<AaaaRecord>();
+            if (options.InterfaceNames.Count > 0 && options.InterfaceAddressFamilies.Count != options.InterfaceNames.Count && options.InterfaceAddressFamilies.Count != 0)
+                throw new Exception("The number of --interface-address-family parameters must be equal to the number of --interface-name parameters. Or be 0 to default to Any.");
+            var interfaces = new Dictionary<string, InterfaceAddressFamily>();
+            for (int i = 0; i < options.InterfaceNames.Count; i++)
+            {
+                interfaces.Add(options.InterfaceNames[i], options.InterfaceAddressFamilies.Count == 0 ? InterfaceAddressFamily.Any : options.InterfaceAddressFamilies[i]);
+            }
+            await foreach (var ip in GetPublicIPs(interfaces))
+            {
+                switch (ip.AddressFamily)
+                {
+                    case AddressFamily.InterNetwork:
+                        aRecordSet.ARecords.Add(new ARecord(ip.ToString()));
+                        break;
+                    case AddressFamily.InterNetworkV6:
+                        aaaaRecordSet.AaaaRecords.Add(new AaaaRecord(ip.ToString()));
+                        break;
+                    default:
+                        continue;
+                }
+            }
+
+            if (aRecordSet.ARecords.Count == 0 && aaaaRecordSet.AaaaRecords.Count == 0)
+                throw new Exception("No IP address found.");
+
+            SetMetadata(aRecordSet);
+            SetMetadata(aaaaRecordSet);
+            aRecordSet.TTL = options.TTL;
+            aaaaRecordSet.TTL = options.TTL;
+
             var (tenantId, clientId, clientSecret) = GetCredentialInfo(options);
             var creds = await ApplicationTokenProvider.LoginSilentAsync(tenantId, clientId, clientSecret);
             var dnsClient = new DnsManagementClient(creds);
             dnsClient.SubscriptionId = options.SubscriptionId;
-
-            var ip = await GetPublicIP();
-            var recordSet = new RecordSet();
-            recordSet.TTL = 3600;
-            recordSet.ARecords = new List<ARecord>();
-            recordSet.ARecords.Add(new ARecord(ip));
-            recordSet.Metadata = new Dictionary<string, string>();
-            recordSet.Metadata.Add("createdBy", "Azure-DynDns (.NET)");
-            recordSet.Metadata.Add("updated", DateTime.Now.ToString());
-            var result = await dnsClient.RecordSets.CreateOrUpdateAsync(options.ResourceGroup, options.Zone, options.Record, RecordType.A, recordSet);
-
-            Console.WriteLine(JsonSerializer.Serialize(result));
+            if (aRecordSet.ARecords.Count > 0)
+            {
+                Console.WriteLine(JsonSerializer.Serialize(await dnsClient.RecordSets.CreateOrUpdateAsync(options.ResourceGroup, options.Zone, options.Record, RecordType.A, aRecordSet)));
+            }
+            if (aaaaRecordSet.AaaaRecords.Count > 0)
+            {
+                Console.WriteLine(JsonSerializer.Serialize(await dnsClient.RecordSets.CreateOrUpdateAsync(options.ResourceGroup, options.Zone, options.Record, RecordType.AAAA, aaaaRecordSet)));
+            }
         }
 
-        public static async Task<string> GetPublicIP()
+        private static void SetMetadata(RecordSet recordSet)
         {
-            var client = new HttpClient();
-            return await client.GetStringAsync("https://ifconfig.me");
+            recordSet.Metadata = new Dictionary<string, string>
+            {
+                { "createdBy", "Azure-DynDns (.NET)" },
+                { "updated", DateTime.Now.ToString() }
+            };
+        }
+
+        public static async IAsyncEnumerable<IPAddress> GetPublicIPs(Dictionary<string, InterfaceAddressFamily> interfaces)
+        {
+            if (interfaces.Count == 0)
+            {
+                var client = new HttpClient();
+                yield return IPAddress.Parse(await client.GetStringAsync("https://ifconfig.me"));
+            }
+
+            foreach (var networkInterface in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                if (!interfaces.TryGetValue(networkInterface.Name, out var requestedAddressFamily))
+                    continue;
+                foreach (var address in networkInterface.GetIPProperties().UnicastAddresses)
+                {
+                    if (!address.Address.IsIPv6Teredo && !address.Address.IsIPv4MappedToIPv6 && !address.Address.IsIPv6SiteLocal && !address.Address.IsIPv6LinkLocal && !address.Address.IsIPv6Multicast && !address.Address.IsIPv6SiteLocal && !address.IsTransient && address.IsDnsEligible && !IsUlaAddress(address.Address))
+                    {
+                        switch (requestedAddressFamily)
+                        {
+                            case InterfaceAddressFamily.Any:
+                                yield return address.Address;
+                                break;
+                            case InterfaceAddressFamily.IPv4:
+                                if (address.Address.AddressFamily == AddressFamily.InterNetwork)
+                                    yield return address.Address;
+                                break;
+                            case InterfaceAddressFamily.IPv6:
+                                if (address.Address.AddressFamily == AddressFamily.InterNetworkV6)
+                                    yield return address.Address;
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+
+                }
+            }
         }
 
         public static (string, string, string) GetCredentialInfo(Options options)
@@ -86,6 +175,12 @@ namespace AzureDynDns
             var clientId = options.ClientId ?? Environment.GetEnvironmentVariable("AZURE_CLIENT_ID");
             var clientSecret = options.ClientSecret ?? Environment.GetEnvironmentVariable("AZURE_CLIENT_SECRET");
             return (tenantId, clientId, clientSecret);
+        }
+
+        private static bool IsUlaAddress(IPAddress address)
+        {
+            var addressString = address.ToString();
+            return addressString.StartsWith("fc00:") || addressString.StartsWith("fd00:");
         }
     }
 }
